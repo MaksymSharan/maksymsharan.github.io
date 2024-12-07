c3163fa507f15b843f75e8f52a0c353cbd38a43d
MaksymSharan <maksimsaran66@gmail.com>
2024-12-07 19:54:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,12 @@ document.addEventListener("scroll", () => {
     const page1 = document.getElementById("page1");
     const page2 = document.getElementById("page2");
 
-    const scrollPosition = window.scrollY;
+    const scrollPos = window.scrollY;
 
-    // Ефект зменшення обкладинки
-    if (scrollPosition < 300) {
-        cover.style.transform = `scale(${1 - scrollPosition / 1000})`;
-    }
+    // Зміна масштабу обкладинки
+    cover.style.transform = `scale(${1 - scrollPos / 1000})`;
 
-    // Плавна поява першої сторінки
-    if (scrollPosition > 300) {
-        page1.style.opacity = (scrollPosition - 300) / 300;
-    }
-
-    // Плавна поява другої сторінки
-    if (scrollPosition > 900) {
-        page2.style.opacity = (scrollPosition - 900) / 300;
-    }
+    // Плавне відкриття сторінок
+    if (scrollPos > 300) page1.style.opacity = (scrollPos - 300) / 300;
+    if (scrollPos > 800) page2.style.opacity = (scrollPos - 800) / 300;
 });
